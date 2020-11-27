ba4ff0c2f171e365b0ee8c7cb199a941c5dea781
Franco Salcedo <franco.salcedo.i3@gmail.com>
2020-11-27 15:32:42 -0500

    Advanced controller

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -19,14 +19,7 @@ class User extends Model
         'address_id',
     ];
 
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-    ];
+    protected $with = ['address'];
 
     public function address()
     {
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -16,3 +16,9 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->get('/users', 'UserController@index');
+$router->post('/users', 'UserController@store');
+$router->get('/users/{$id}', 'UserController@show');
+$router->put('/users/{$id}', 'UserController@update');
+$router->delete('/users/{$id}', 'UserController@delete');
